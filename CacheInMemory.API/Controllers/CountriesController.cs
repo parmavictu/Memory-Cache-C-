42f65a2b5e06f896283f22241ffa286434a7330a
Victor Parma <victorparma@hotmail.com>
2020-09-24 20:08:04 -0300

    finished

---
+++ CacheInMemory.API/Controllers/CountriesController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using CacheInMemory.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace CacheInMemory.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CountriesController : ControllerBase
+    {
+        private readonly ILogger<CountriesController> logger;
+        private readonly IMemoryCache memoryCache;
+
+        private const string COUNTRIES_KEY = "Countries";
+        private const string REST_COUNTRIES_URL = "https://restcountries.eu/rest/v2/all";
+
+        public CountriesController(ILogger<CountriesController> logger, IMemoryCache cache)
+        {
+            this.logger = logger;
+            this.memoryCache = cache;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetCountries()
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+
+            if (memoryCache.TryGetValue(COUNTRIES_KEY, out object countriesObject))
+            {
+                logger.LogInformation("Getting countries from MemoryCache");
+
+                sw.Stop();
+                logger.LogInformation($"Time spent in miliseconds: {sw.Elapsed.TotalMilliseconds}");
+
+                return Ok(countriesObject);
+            }
+            else
+            {
+                
+                logger.LogInformation("Getting countries from API");
+
+                using (var httpClient = new HttpClient())
+                {
+                    var response = await httpClient.GetAsync(REST_COUNTRIES_URL);
+
+                    var responseData = await response.Content.ReadAsStringAsync();
+               
+                    var countries = JsonConvert.DeserializeObject<List<Country>>(responseData).Take(10);
+
+                    var memoryCacheEntryOptions = new MemoryCacheEntryOptions
+                    {
+                        AbsoluteExpirationRelativeToNow = TimeSpan.FromSeconds(3600),
+                        SlidingExpiration = TimeSpan.FromSeconds(1200),
+                        Size = 10
+                    };
+
+                    memoryCache.Set(COUNTRIES_KEY, countries, memoryCacheEntryOptions);
+
+                    sw.Stop();
+                    logger.LogInformation($"Time spent in miliseconds: {sw.Elapsed.TotalMilliseconds}");
+
+                    return Ok(countries);
+                }
+            }
+        }
+    }
+}
---
+++ CacheInMemory.API/Models/Country.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace CacheInMemory.API.Models
+{
+    public class Country
+    {
+        public string Name { get; set; }
+        public string Capital { get; set; }
+        public string Region { get; set; }
+        public string Denonym { get; set; }
+    }
+}
